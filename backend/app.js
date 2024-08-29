ee72d686392232e15e9c35fbe61b83ca9df8b8cb
Pasindya Chavindee <151112355+Pasindya@users.noreply.github.com>
2024-08-29 22:24:11 +0530

    Update app.js
    updated with booking crud

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const router = require("./Route/BookingRoute");
+const router = require("./Route/BookingRoute")
 
 const app = express();
 
-app.use("/",(req, res, next) => {
-    res.send("It is working");
-})
-mongoose.connect("mongodb+srv://surfdeck:surfdeck1234@cluster0.kcpia.mongodb.net/")
-
-
-/*//middleware
+//middleware
 app.use(express.json());
 app.use("/bookings",router);
 
@@ -21,4 +15,4 @@ mongoose.connect("mongodb+srv://surfdeck:surfdeck1234@cluster0.kcpia.mongodb.net
 .then(() => {
     app.listen(5000);
 })
-.catch((err) => console.log((err)));*/
+.catch((err) => console.log((err)));
