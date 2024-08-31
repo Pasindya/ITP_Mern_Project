fc0e2f6cdc1489c124b8f795508b0d739948d10a
tharindu9216 <tharindukaveeshaperera9216@gmail.com>
2024-08-31 13:29:43 +0530

    update and delete part backend

--- backend/Conrollers/StaffControl.js
+++ backend/Conrollers/StaffControl.js
@@ -1,6 +1,6 @@
 const Staff = require("../Model/StaffModel");
 
-const getAllStaff = async (req, res, next) => {  // Changed the second 'req' to 'res'
+const getAllStaff = async (req, res, next) => {
     let staff;
     try {
         staff = await Staff.find();
@@ -13,50 +13,93 @@ const getAllStaff = async (req, res, next) => {  // Changed the second 'req' to 
     return res.status(200).json({ staff });
 };
 
-//data Insert 
-const addStaff = async (req,res,next) => {
-    const {name,gmail,age,address,experience,password} = req.body;
+// Data Insert
+const addStaff = async (req, res, next) => {
+    const { name, gmail, age, address, experience, password } = req.body;
 
     let staff;
-   
+
     try {
-        staff = new Staff({name,gmail,age,address,experience,password});
+        staff = new Staff({ name, gmail, age, address, experience, password });
         await staff.save();
-    }catch (err) {
+    } catch (err) {
         console.log(err);
     }
-    
-    // not insert staff member
-    if (!staff){
-        return res.status(404).json({message:"unable to add staff member"});
 
+    // If unable to insert staff member
+    if (!staff) {
+        return res.status(404).json({ message: "Unable to add staff member" });
     }
     return res.status(200).json({ staff });
-
 };
 
-//Get by id
-const getById = async (req,res,next) => {
-
+// Get by ID
+const getById = async (req, res, next) => {
     const id = req.params.id;
-
     let staff;
 
     try {
         staff = await Staff.findById(id);
+    } catch (err) {
+        console.log(err);
+    }
+
+    // If staff member not found
+    if (!staff) {
+        return res.status(404).json({ message: "Staff member not found" });
+    }
+    return res.status(200).json({ staff });
+};
+
+// Update staff member
+const updateStaff = async (req, res, next) => {
+    const id = req.params.id;
+    const { name, gmail, age, address, experience, password } = req.body;
+    let staff;
+
+    try {
+        staff = await Staff.findByIdAndUpdate(id, {
+            name,
+            gmail,
+            age,
+            address,
+            experience,
+            password,
+        });
+        staff = await staff.save();
+    } catch (err) {
+        console.log(err);
+    }
+
+    // If unable to update staff member details
+    if (!staff) {
+        return res.status(404).json({ message: "Unable to update staff member details" });
+    }
+    return res.status(200).json({ staff });
+};
+
+   //Delete staff memeber details 
+   const deleteStaff = async (req,res,next) => {
+    const id = req.params.id;
+
+    let staff;
+
+    try{
+        staff = await Staff.findByIdAndDelete(id)
     }catch (err) {
         console.log(err);
     }
-    //not available users
-    if (!staff){
-        return res.status(404).json({message:"unable to add staff member"});
-
+    //unable to delete
+    if (!staff) {
+        return res.status(404).json({ message: "Unable to delete staff member details" });
     }
     return res.status(200).json({ staff });
-}
+   }
 
-
-
+// Corrected export
 exports.getAllStaff = getAllStaff;
 exports.addStaff = addStaff;
 exports.getById = getById;
+exports.updateStaff = updateStaff;
+exports.deleteStaff = deleteStaff;
+  
--- backend/Route/StaffRoute.js
+++ backend/Route/StaffRoute.js
@@ -8,6 +8,8 @@ const StaffControl = require("../Conrollers/StaffControl");
 router.get("/",StaffControl.getAllStaff);
 router.post("/",StaffControl.addStaff);
 router.get("/:id",StaffControl.getById);
+router.put("/:id",StaffControl. updateStaff);
+router.delete("/:id",StaffControl. deleteStaff);
 
 
 //export
