ced0e2baac5adc4c01b1df76ed26c52ee30d782e
Pasindya Chavindee <151112355+Pasindya@users.noreply.github.com>
2024-09-02 23:20:16 +0530

    updated paymentcontrol

--- backend/Conrollers/PaymentControl.js
+++ backend/Conrollers/PaymentControl.js
@@ -33,23 +33,66 @@ const addPayment = async (req,res,next) => {
 };
 
 // Get by ID
-const getPaymentById = async (req,res,next) =>{
-    const id = req.params.id;
+const getById = async (req,res,next) =>{
+    const  {id } = req.params;
     try {
-        const payment = await PaymentModel.findById(id);
-        if (!payment) return res.status(404).json({message: "Payment not found"});
-        res.json(payment);
+        const paymentDoc = await Payment.findById(id);
+        if (!paymentDoc) {
+             return res.status(404).json({message: "Payment not found"});
+        }
+        return res.status(200).json({payment: paymentDoc});
 
     } catch (error) {
-        res.status(500).send(error.message);
+        console.error("Error fetchin payment:", error);
+        return res.status(500).json({message: "Server error"});
     }
 };
 
+// Update payment details
+const updatePayment = async (req,res,next) => {
+    const {id} = req.params;
+    const {Fullname, email, Address, cardType, cardNumber, expirationMonth, expirationYear, cvv} = req.body;
+    
+    let updatedPayment;
+    
+    try {
+        updatedPayment = await Payment.findByIdAndUpdate(id, {Fullname, email, Address, cardType, cardNumber, expirationMonth, expirationYear, cvv}, {new: true}); // return the updated documnet
+
+        if (!updatedPayment) {
+            return res.status(404).json({message: "Unable to update payment details"});
+        }
+        return res.status(200).json({payment: updatedPayment});
+        
+    } catch (error) {
+        console.error("Error updating payment:", error);
+        return res.status(500).json({message: "Server error"});
+    }
+};
+
+// Delete Payment
+const deletePayment = async (req,res,next) => {
+    const {id} = req.params;
+
+    let deletedPayment;
+
+    try {
+        deletedPayment = await Payment.findByIdAndDelete(id); // find and delete the payment by id
+        if (!deletedPayment) {
+            return res.status(404).json({message: "Unable to delete payment details"});
+        }
+        return res.status(200).json({message: "Payment deleted successfully", payment: deletedPayment});
+
+    } catch (error) {
+        console.error("Error deleting payment:", error);
+
+        return res.status(500).json({message: "Server error"});
+    }
+};
+
+
+
 exports.getAllPayment = getAllPayment;
 exports.addPayment = addPayment;
-exports.getPaymentById = getPaymentById;
-
-
-
-
- 
+exports.getById = getById;
+exports.updatePayment = updatePayment;
+exports.deletePayment = deletePayment;
