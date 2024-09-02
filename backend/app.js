e22b465c603fc10083f6b65a51fff1113b9fab9c
Pasindya Chavindee <151112355+Pasindya@users.noreply.github.com>
2024-09-02 23:19:54 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,9 @@ const cors = require("cors");
 // Import routes with unique variable names
 const bookingRouter = require("./Route/BookingRoute");
 const staffRouter = require("./Route/StaffRoute");
-const studentRouter = require("./Route/StudentRoute");
 const paymentRouter = require("./Route/PaymentRoute");
+
+
 
 
 const app = express();
@@ -18,15 +19,15 @@ app.use(cors());
 // Apply the routers to their respective paths
 app.use("/bookings", bookingRouter);
 app.use("/staff", staffRouter);
-app.use("/student", studentRouter);
-app.use("/payments",paymentRouter);
+
 
 // Connect to MongoDB and start the server
 mongoose.connect("mongodb+srv://surfdeck:surfdeck1234@cluster0.kcpia.mongodb.net/")
     .then(() => console.log("Connected to MongoDB"))
     .then(() => {
-        app.listen(5000, () => {
-            console.log("Server is running on port 5000");
-        });
+        app.listen(5001, () => {
+            console.log("Server is running on port 5001");
+          });
+          
     })
     .catch((err) => console.log(err));
